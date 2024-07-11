ba9c3c2329ba315ddfb8ac68b7223db18bcc9907
Gianluca Carra <contact@giancarra.com>
2024-07-10 20:56:33 -0400

    Initial commit

---
+++ Petfolio.Application/UseCases/Pets/Register/RegisterPetUseCase.cs
@@ -0,0 +1,15 @@
+﻿using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pets.Register;
+public class RegisterPetUseCase
+{
+    public ResponseRegisterPetJson Execute(RequestPetJson request)
+    {
+        return new ResponseRegisterPetJson
+        {
+            Id = 0,
+            Name = request.Name
+        };
+    }
+}
